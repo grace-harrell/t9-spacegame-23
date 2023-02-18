6fbbf9454caea432019cdc2bff9da7396f6f4c96
Grace Harrell <graceharrell003@gmail.com>
2023-02-18 07:12:59 -0700

    added astroid

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ VEL = 5
 
 WIDTH, HEIGHT = 1200, 800
 SPACE_STATION_WIDTH, SPACE_STATION_HEIGHT = 300, 300
+ASTROID_WIDTH, ASTROID_HEIGHT = 200, 200
 
 BACKGROUND_IMG = pygame.image.load(os.path.join('Assets', 'spacegame_background.png'))
 BACKGROUND = pygame.transform.scale(BACKGROUND_IMG, (WIDTH, HEIGHT))
@@ -14,17 +15,21 @@ BACKGROUND = pygame.transform.scale(BACKGROUND_IMG, (WIDTH, HEIGHT))
 SPACE_STATION_IMG = pygame.image.load(os.path.join('Assets', 'spacestation_sprite.png'))
 SPACE_STATION = pygame.transform.scale(SPACE_STATION_IMG, (SPACE_STATION_WIDTH, SPACE_STATION_HEIGHT))
 
+ASTROID_IMG = pygame.image.load(os.path.join('Assets', 'astroid_sprite.png'))
+ASTROID = pygame.transform.scale(ASTROID_IMG, (ASTROID_WIDTH, ASTROID_HEIGHT))
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Game")
 
-def draw_window(player):
+def draw_window(player, space_trash):
     WIN.blit(BACKGROUND,(0,0))
     WIN.blit(SPACE_STATION, (player.x, player.y))
+    WIN.blit(ASTROID, (space_trash.x, space_trash.y))
 
     pygame.display.update()
 
 def main():
+    space_trash = pygame.Rect(1300, 150, ASTROID_WIDTH, ASTROID_HEIGHT)
     player = pygame.Rect(150,300, SPACE_STATION_WIDTH, SPACE_STATION_HEIGHT)
 
     clock = pygame.time.Clock()
@@ -35,6 +40,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+        space_trash.x -= 1
+
         keys_pressed = pygame.key.get_pressed()
 
         if keys_pressed[pygame.K_w]:  # MOVE UP
@@ -43,7 +50,7 @@ def main():
         if keys_pressed[pygame.K_s]: # MOVE DOWN
             player.y += VEL
 
-        draw_window(player)
+        draw_window(player, space_trash)
     pygame.quit()
 
 if __name__ == "__main__":
